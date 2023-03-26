620f98999f6e33f30d4fc09ed4eea1caf544355a
nfurno <nicholas.furno@gmail.com>
2023-03-26 02:22:14 -0400

    fixes

--- terraform/modules/authentication/main.tf
+++ terraform/modules/authentication/main.tf
@@ -20,7 +20,7 @@ resource "aws_cognito_user_pool_client" "this" {
 }
 
 resource "aws_cognito_user_pool_domain" "this" {
-  domain       = "tasktrack.auth.${var.aws_region}.amazoncognito.com"
+  domain       = "tasktrack"
   user_pool_id = aws_cognito_user_pool.this.id
 }
 
