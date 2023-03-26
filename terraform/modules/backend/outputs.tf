13130de0e6fadd7640abafa2bc4b9ced5a0dde71
nfurno <nicholas.furno@gmail.com>
2023-03-26 01:46:16 -0400

    fixes

--- terraform/modules/backend/outputs.tf
+++ terraform/modules/backend/outputs.tf
@@ -1,4 +1,4 @@
-output "api_gateway_url" {
-  description = "The URL of the deployed API Gateway"
-  value       = "https://${aws_api_gateway_rest_api.this.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.deployment.stage_name}"
-}
+#output "api_gateway_url" {
+#  description = "The URL of the deployed API Gateway"
+#  value       = "https://${aws_api_gateway_rest_api.this.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.deployment.stage_name}"
+#}
